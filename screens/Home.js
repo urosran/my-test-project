ff451d0f0f69c6c2720e4c0ee1f6006928e545ba
Uros Randelovic <uros.randelovic@gmail.com>
2018-11-28 23:46:49 -0500

    first commit ?

--- screens/Home.js
+++ screens/Home.js
@@ -12,11 +12,8 @@ import Input from "./Input"
 import { fromJS } from 'immutable';
 
 class Home extends React.Component {
-    // static navigationOptions = {
-    //   title: 'Welcome',
-    // };
+  
     render() {
-    //   const {navigate} = this.props.navigation;
       return (
         <View> 
          <Appbar style={styles1.bottom}>
@@ -36,48 +33,7 @@ class Home extends React.Component {
   }
 
   export default Home
-// state = {
-//     data: fromJS({
-//         person:{
-//             name: "",
-//             surname: ""
-//         }    
-//     })
-//   };
 
-
-// const Home = ({ navigation }) => (
-    
-//     <View> 
-//         <Appbar style={styles1.bottom}>
-        
-//             <Appbar.Action icon="map" onPress={()=>{ navigation.navigate('Map')}} />
-//             <Appbar.Action icon={`favorite`} onPress={() => console.log('Pressed mail')} />
-//             {/* <Appbar.Action icon="label" onPress={() => console.log('Pressed label')} />
-//             <Appbar.Action icon="delete" onPress={() => console.log('Pressed delete')} /> */}
-//          </Appbar>
-//             <Input/>
-
-//         <ScrollView>
-
-//         </ScrollView>
-     
-//      </View>    
-//     )
-
-
-//    Home.navigationOptions = ({ navigation }) => ({
-//     title: "NameGuru",
-//     headerRight: (
-//       <Button
-//         icon="language"
-//         mode="contained"
-//         title="Map"
-//         onPress={()=>{ navigation.navigate('Map')}}
-//         // disabled={navigation.getParam('stock') === 0}
-//       />
-//     )
-//   });
   
   const styles1 = StyleSheet.create({
     bottom: {
